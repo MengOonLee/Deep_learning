14e0994f67be77ac3c91166e040da3d08144fff4
MengOonLee <darklemon2000@gmail.com>
2022-07-18 08:56:53 +0800

    Revise part 1

---
+++ TensorFlow2/Tutorial/Getting_started_TensorFlow2/Saving_loading_models/pip_install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+pip install -qU pip wheel
+pip install -qU numpy pandas matplotlib
+pip install -qU scikit-learn
---
+++ TensorFlow2/Tutorial/Getting_started_TensorFlow2/Validation_regularisation_callbacks/pip_install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+pip install -U pip wheel
+pip install -U numpy pandas matplotlib
+pip install -U scikit-learn
