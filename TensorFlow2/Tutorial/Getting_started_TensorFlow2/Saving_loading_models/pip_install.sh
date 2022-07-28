e826eacd7f50d2d5bba65d45976f32e3c2aa7ce2
MengOonLee <darklemon2000@gmail.com>
2022-07-28 09:12:31 +0800

    Tensorflow2: Save and load model

--- TensorFlow2/Tutorial/Getting_started_TensorFlow2/Saving_loading_models/pip_install.sh
+++ TensorFlow2/Tutorial/Getting_started_TensorFlow2/Saving_loading_models/pip_install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-pip install -qU pip wheel
-pip install -qU numpy pandas matplotlib
-pip install -qU scikit-learn
+pip install --no-cache-dir -U pip wheel
+pip install --no-cache-dir -U numpy pandas matplotlib seaborn
+pip install --no-cache-dir -U scikit-learn
+pip install --no-cache-dir -U tensorflow_hub
+pip check
