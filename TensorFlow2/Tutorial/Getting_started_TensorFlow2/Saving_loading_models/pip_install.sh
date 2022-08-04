e1b3680983c5b93b09d0b222df5640fff86fc35a
MengOonLee <darklemon2000@gmail.com>
2022-08-04 20:55:03 +0800

    Tensorflow2: Capstone project

--- TensorFlow2/Tutorial/Getting_started_TensorFlow2/Saving_loading_models/pip_install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-pip install --no-cache-dir -U pip wheel
-pip install --no-cache-dir -U numpy pandas matplotlib seaborn
-pip install --no-cache-dir -U scikit-learn
-pip install --no-cache-dir -U tensorflow_hub
-pip check
--- TensorFlow2/Tutorial/Getting_started_TensorFlow2/Validation_regularisation_callbacks/pip_install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-pip install -U pip wheel
-pip install -U numpy pandas matplotlib
-pip install -U scikit-learn
